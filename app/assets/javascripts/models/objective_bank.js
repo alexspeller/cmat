097764291cb39def723963be8312bf23c396ca02
Bowen Masco <bowen@codingfoo.com>
2013-08-01 21:07:39 -0600

    asdf

--- app/assets/javascripts/models/objective_bank.js
+++ app/assets/javascripts/models/objective_bank.js
@@ -9,26 +9,22 @@ var ObjectiveBank = ModelBase.extend({
 
 var adapter = DS.Adapter.extend({
   find: function(store, type, id) {
-    $.getJSON('https://oki-dev.mit.edu/handcar/services/learning/objectivebanks/'+id).then( function(json) {
+    return $.getJSON('https://oki-dev.mit.edu/handcar/services/learning/objectivebanks/'+id).then( function(json) {
       store.load(type, json);
     });
   },
   findQuery: function(store, type, query, recordArray) {
-    // $.getJSON('https://oki-dev.mit.edu/handcar/services/learning/objectivebanks').then( function(objectivebanks) {
-    //   //console.log(objectivebanks);
-    //   objectivebanks.forEach(function(objectivebank){
-    //     //console.log(objectivebank);
-    //   });
-
-    //     console.log(adapter);
-    //     console.log(since);
-    //     store.load(type, objectivebank[0]);
       adapter = this;
 
-      return $.getJSON('https://oki-dev.mit.edu/handcar/services/learning/objectivebanks').then(function(json){
-        console.log(json);
-        console.log(json.data);
-        adapter.didFindQuery(store, type, json, recordArray);
+
+      return $.getJSON('https://oki-dev.mit.edu/handcar/services/learning/objectivebanks').then(function(response){
+        var banks = Em.A();
+        console.log(response);
+        response.forEach(function (bank) {
+          banks.pushObject(App.ObjectiveBank.createRecord(bank));
+        });
+        console.log(banks);
+        return banks;
       });
   }
 });
